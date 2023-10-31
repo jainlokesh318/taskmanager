9672d4f064800a2055d32a5d48c90b9705fd1571
Lokesh Jain <lokesh.jain@juspay.in>
2023-10-31 18:20:02 +0530

    add task to list

--- src/components/TaskForm.js
+++ src/components/TaskForm.js
@@ -1,26 +1,37 @@
 import { useState } from "react"
+import { v4 as uuid } from 'uuid';
 
 function TextInput({ type, id, value, setValue, placeholder }) {
     return <input className="border p-2" type={type} id={id} value={value} onChange={setValue} placeholder={placeholder} />
 }
 
-export default function TaskForm({ handleFormSubmission }) {
+export default function TaskForm({ handleSubmit, taskToEdit }) {
     const [title, setTitle] = useState("")
     const [description, setDescription] = useState("")
     const [status, setStatus] = useState("")
+    const areFieldsPopulated = title.length > 0 && description.length > 0
 
-    const isFormDisabled = title.length > 0 && description.length > 0
+    const handleFormSubmission = (e) => {
+        e.preventDefault();
+        const task = {
+            id: uuid(),
+            title, description, status
+        }
+        handleSubmit(task);
+    }
 
-    return <form onSubmit={handleFormSubmission}>
-        <div className="grid grid-cols-2">
-            <label htmlFor="title">Title</label>
-            <TextInput type="text" id="title" value={title} placeholder="Enter title" setValue={e => setTitle(e.target.value)} />
+    return (
+        <form onSubmit={handleFormSubmission} className="flex flex-col gap-4 m-2">
+            <div className="grid grid-cols-2">
+                <label htmlFor="title">Title</label>
+                <TextInput type="text" id="title" value={title} placeholder="Enter title" setValue={e => setTitle(e.target.value)} />
 
-        </div>
-        <div className="grid grid-cols-2">
-            <label htmlFor="title">Description</label>
-            <TextInput type="text" value={description} placeholder="Enter Description" setValue={e => setDescription(e.target.value)} />
-        </div>
-        <button disabled={true} type="submit" className="border rounded px-4">Add</button>
-    </form>
+            </div>
+            <div className="grid grid-cols-2">
+                <label htmlFor="title">Description</label>
+                <TextInput type="text" value={description} placeholder="Enter Description" setValue={e => setDescription(e.target.value)} />
+            </div>
+            <button disabled={!areFieldsPopulated} type="submit" className="border rounded px-4 disabled:opacity-50 disabled:cursor-not-allowed bg-orange-300">Add Task</button>
+        </form>
+    )
 }
--- src/components/TaskManager.js
+++ src/components/TaskManager.js
@@ -4,9 +4,8 @@ import TaskForm from "./TaskForm";
 export default function TaskManager() {
     let [tasksList, setTasksList] = useState([])
     let [taskToEdit, setTaskToEdit] = useState(undefined);
-    const handleSubmit = (e) => {
-        e.preventDefault()
-        console.log("form")
+    const handleSubmit = (task) => {
+        setTasksList(prev => [...prev, task])
     }
     return <TaskForm handleFormSubmission={handleSubmit} />
 }
